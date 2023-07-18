0afa02a6283ca26591e38e1254001debea3aecf7
Moffein <22742925+Moffein@users.noreply.github.com>
2023-07-18 15:46:05 -0700

    Add project files.

---
+++ ArtificerMikaVoiceover/ArtificerMikaVoiceoverPlugin.cs
@@ -0,0 +1,235 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using RoR2;
+using RoR2.Audio;
+using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.Networking;
+using ArtificerMikaVoiceover.Components;
+using ArtificerMikaVoiceover.Modules;
+
+namespace ArtificerMikaVoiceover
+{
+    [BepInDependency("com.rune580.riskofoptions", BepInDependency.DependencyFlags.SoftDependency)]
+    [BepInDependency("com.Alicket.MisonoMikaArtificer")]
+    [BepInPlugin("com.Schale.ArtificerMikaVoiceover", "ArtificerMikaVoiceover", "1.0.0")]
+    public class ArtificerMikaVoiceoverPlugin : BaseUnityPlugin
+    {
+        public static ConfigEntry<bool> enableVoicelines;
+        private static SurvivorDef mageSurvivorDef = Addressables.LoadAssetAsync<SurvivorDef>("RoR2/Base/Merc/Mage.asset").WaitForCompletion(); //Why is this Merc?
+        public static bool playedSeasonalVoiceline = false;
+        public static AssetBundle assetBundle;
+
+        public void Awake()
+        {
+            Files.PluginInfo = this.Info;
+            BaseVoiceoverComponent.Init();
+            RoR2.RoR2Application.onLoad += OnLoad;
+            new Content().Initialize();
+
+            using (var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("ArtificerMikaVoiceover.banditmikavoiceoverbundle"))  //Just reuse the icon from that one.
+            {
+                assetBundle = AssetBundle.LoadFromStream(stream);
+            }
+
+            InitNSE();
+
+            enableVoicelines = base.Config.Bind<bool>(new ConfigDefinition("Settings", "Enable Voicelines"), true, new ConfigDescription("Enable voicelines when using the Artificer Mika Skin."));
+            if (BepInEx.Bootstrap.Chainloader.PluginInfos.ContainsKey("com.rune580.riskofoptions"))
+            {
+                RiskOfOptionsCompat();
+            }
+        }
+
+        private void Start()
+        {
+            SoundBanks.Init();
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining | MethodImplOptions.NoOptimization)]
+        private void RiskOfOptionsCompat()
+        {
+            RiskOfOptions.ModSettingsManager.AddOption(new RiskOfOptions.Options.CheckBoxOption(enableVoicelines));
+            RiskOfOptions.ModSettingsManager.SetModIcon(assetBundle.LoadAsset<Sprite>("texModIcon"));
+        }
+
+        private void OnLoad()
+        {
+            bool foundSkin = false;
+            SkinDef[] skins = SkinCatalog.FindSkinsForBody(BodyCatalog.FindBodyIndex("MageBody"));
+            foreach (SkinDef skinDef in skins)
+            {
+                if (skinDef.name == "MikaModDef")
+                {
+                    foundSkin = true;
+                    ArtiMikaVoiceoverComponent.requiredSkinDefs.Add(skinDef);
+                    break;
+                }
+            }
+
+            if (!foundSkin)
+            {
+                Debug.LogError("ArtificerMikaVoiceover: Artificer Mika SkinDef not found. Voicelines will not work!");
+            }
+            else
+            {
+                On.RoR2.CharacterBody.Start += AttachVoiceoverComponent;
+
+                On.RoR2.SurvivorMannequins.SurvivorMannequinSlotController.RebuildMannequinInstance += (orig, self) =>
+                {
+                    orig(self);
+                    if (self.currentSurvivorDef == mageSurvivorDef)
+                    {
+                        //Loadout isn't loaded first time this is called, so we need to manually get it.
+                        //Probably not the most elegant way to resolve this.
+                        if (self.loadoutDirty)
+                        {
+                            if (self.networkUser)
+                            {
+                                self.networkUser.networkLoadout.CopyLoadout(self.currentLoadout);
+                            }
+                        }
+
+                        //Check SkinDef
+                        BodyIndex bodyIndexFromSurvivorIndex = SurvivorCatalog.GetBodyIndexFromSurvivorIndex(self.currentSurvivorDef.survivorIndex);
+                        int skinIndex = (int)self.currentLoadout.bodyLoadoutManager.GetSkinIndex(bodyIndexFromSurvivorIndex);
+                        SkinDef safe = HG.ArrayUtils.GetSafe<SkinDef>(BodyCatalog.GetBodySkins(bodyIndexFromSurvivorIndex), skinIndex);
+                        if (ArtiMikaVoiceoverComponent.requiredSkinDefs.Contains(safe))
+                        {
+                            bool played = false;
+                            if (!playedSeasonalVoiceline)
+                            {
+                                if (System.DateTime.Today.Month == 1 && System.DateTime.Today.Day == 1)
+                                {
+                                    Util.PlaySound("Play_ArtiMika_Lobby_Newyear", self.mannequinInstanceTransform.gameObject);
+                                    played = true;
+                                }
+                                else if (System.DateTime.Today.Month == 5 && System.DateTime.Today.Day == 8)
+                                {
+                                    Util.PlaySound("Play_ArtiMika_Lobby_bday", self.mannequinInstanceTransform.gameObject);
+                                    played = true;
+                                }
+                                else if (System.DateTime.Today.Month == 10 && System.DateTime.Today.Day == 31)
+                                {
+                                    Util.PlaySound("Play_ArtiMika_Lobby_Halloween", self.mannequinInstanceTransform.gameObject);
+                                    played = true;
+                                }
+                                else if (System.DateTime.Today.Month == 12 && System.DateTime.Today.Day == 25)
+                                {
+                                    Util.PlaySound("Play_ArtiMika_Lobby_xmas", self.mannequinInstanceTransform.gameObject);
+                                    played = true;
+                                }
+
+                                if (played)
+                                {
+                                    playedSeasonalVoiceline = true;
+                                }
+                            }
+                            if (!played)
+                            {
+                                if (Util.CheckRoll(5f))
+                                {
+                                    Util.PlaySound("Play_ArtiMika_TitleDrop", self.mannequinInstanceTransform.gameObject);
+                                }
+                                else
+                                {
+                                    Util.PlaySound("Play_ArtiMika_Lobby", self.mannequinInstanceTransform.gameObject);
+                                }
+                            }
+                        }
+                    }
+                };
+            }
+            ArtiMikaVoiceoverComponent.ScepterIndex = ItemCatalog.FindItemIndex("ITEM_ANCIENT_SCEPTER");
+
+            //Shrine Fail Hook
+            On.RoR2.ShrineChanceBehavior.AddShrineStack += (orig, self, activator) =>
+            {
+                int successes = self.successfulPurchaseCount;
+                orig(self, activator);
+
+                //No change in successes = fail
+                if (NetworkServer.active && self.successfulPurchaseCount == successes)
+                {
+                    if (activator)
+                    {
+                        ArtiMikaVoiceoverComponent vo = activator.GetComponent<ArtiMikaVoiceoverComponent>();
+                        if (vo)
+                        {
+                            vo.PlayShrineOfChanceFailServer();
+                        }
+                    }
+                }
+            };
+
+            On.EntityStates.Mage.Weapon.BaseChargeBombState.OnEnter += (orig, self) =>
+            {
+                orig(self);
+
+                ArtiMikaVoiceoverComponent amvc = self.GetComponent<ArtiMikaVoiceoverComponent>();
+                if (amvc)
+                {
+                    amvc.PlayBeginCharge();
+                }
+            };
+
+            On.EntityStates.Mage.Weapon.BaseThrowBombState.OnEnter += (orig, self) =>
+            {
+                orig(self);
+
+                ArtiMikaVoiceoverComponent amvc = self.GetComponent<ArtiMikaVoiceoverComponent>();
+                if (amvc)
+                {
+                    amvc.PlayReleaseCharge();
+                }
+            };
+        }
+
+        private void AttachVoiceoverComponent(On.RoR2.CharacterBody.orig_Start orig, CharacterBody self)
+        {
+            orig(self);
+            if (self)
+            {
+                if (ArtiMikaVoiceoverComponent.requiredSkinDefs.Contains(SkinCatalog.GetBodySkinDef(self.bodyIndex, (int)self.skinIndex)))
+                {
+                    BaseVoiceoverComponent existingVoiceoverComponent = self.GetComponent<BaseVoiceoverComponent>();
+                    if (!existingVoiceoverComponent) self.gameObject.AddComponent<ArtiMikaVoiceoverComponent>();
+                }
+            }
+        }
+
+        private void InitNSE()
+        {
+            ArtiMikaVoiceoverComponent.nseShout = ScriptableObject.CreateInstance<NetworkSoundEventDef>();
+            ArtiMikaVoiceoverComponent.nseShout.eventName = "Play_ArtiMika_Shout";
+            Content.networkSoundEventDefs.Add(ArtiMikaVoiceoverComponent.nseShout);
+
+            ArtiMikaVoiceoverComponent.nseBuffSelf = ScriptableObject.CreateInstance<NetworkSoundEventDef>();
+            ArtiMikaVoiceoverComponent.nseBuffSelf.eventName = "Play_ArtiMika_BuffSelf";
+            Content.networkSoundEventDefs.Add(ArtiMikaVoiceoverComponent.nseBuffSelf);
+
+            ArtiMikaVoiceoverComponent.nseBlock = ScriptableObject.CreateInstance<NetworkSoundEventDef>();
+            ArtiMikaVoiceoverComponent.nseBlock.eventName = "Play_ArtiMika_Blocked";
+            Content.networkSoundEventDefs.Add(ArtiMikaVoiceoverComponent.nseBlock);
+
+            ArtiMikaVoiceoverComponent.nseShrineFail = ScriptableObject.CreateInstance<NetworkSoundEventDef>();
+            ArtiMikaVoiceoverComponent.nseShrineFail.eventName = "Play_ArtiMika_ShrineFail";
+            Content.networkSoundEventDefs.Add(ArtiMikaVoiceoverComponent.nseShrineFail);
+
+            /*ArtiMikaVoiceoverComponent.nseCommonSkill = ScriptableObject.CreateInstance<NetworkSoundEventDef>();
+            ArtiMikaVoiceoverComponent.nseCommonSkill.eventName = "Play_ArtiMika_CommonSkill";
+            Content.networkSoundEventDefs.Add(ArtiMikaVoiceoverComponent.nseCommonSkill);*/
+
+            ArtiMikaVoiceoverComponent.nseTactical = ScriptableObject.CreateInstance<NetworkSoundEventDef>();
+            ArtiMikaVoiceoverComponent.nseTactical.eventName = "Play_ArtiMika_TacticalAction";
+            Content.networkSoundEventDefs.Add(ArtiMikaVoiceoverComponent.nseTactical);
+
+            ArtiMikaVoiceoverComponent.nseIceWall = ScriptableObject.CreateInstance<NetworkSoundEventDef>();
+            ArtiMikaVoiceoverComponent.nseIceWall.eventName = "Play_ArtiMika_IceWall";
+            Content.networkSoundEventDefs.Add(ArtiMikaVoiceoverComponent.nseIceWall);
+        }
+    }
+}
---
+++ ArtificerMikaVoiceover/Components/ArtiMikaVoiceoverComponent.cs
@@ -0,0 +1,181 @@
+﻿using RoR2;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace ArtificerMikaVoiceover.Components
+{
+    public class ArtiMikaVoiceoverComponent : BaseVoiceoverComponent
+    {
+        public static List<SkinDef> requiredSkinDefs = new List<SkinDef>();
+        public static ItemIndex ScepterIndex;
+
+        public static NetworkSoundEventDef nseBlock;
+        public static NetworkSoundEventDef nseShrineFail;
+        public static NetworkSoundEventDef nseShout;
+        public static NetworkSoundEventDef nseIceWall;
+        public static NetworkSoundEventDef nseBuffSelf;
+        //public static NetworkSoundEventDef nseCommonSkill;
+        public static NetworkSoundEventDef nseTactical;
+
+        private float levelCooldown = 0f;
+        private float blockedCooldown = 0f;
+        private float shrineOfChanceFailCooldown = 0f;
+        private float utilityCooldown = 0f;
+        private float specialCooldown = 0f;
+
+        private bool acquiredScepter = false;
+
+        protected override void Awake()
+        {
+            spawnVoicelineDelay = 3f;
+            if (Run.instance && Run.instance.stageClearCount == 0)
+            {
+                spawnVoicelineDelay = 6.5f;
+            }
+            base.Awake();
+        }
+
+        protected override void FixedUpdate()
+        {
+            base.FixedUpdate();
+            if (levelCooldown > 0f) levelCooldown -= Time.fixedDeltaTime;
+            if (shrineOfChanceFailCooldown > 0f) shrineOfChanceFailCooldown -= Time.fixedDeltaTime;
+            if (blockedCooldown > 0f) blockedCooldown -= Time.fixedDeltaTime;
+            if (utilityCooldown > 0f) utilityCooldown -= Time.fixedDeltaTime;
+            if (specialCooldown > 0f) specialCooldown -= Time.fixedDeltaTime;
+        }
+
+        public override void PlayDamageBlockedServer()
+        {
+            if (!NetworkServer.active || blockedCooldown > 0f) return;
+            bool played = TryPlayNetworkSound(nseBlock, 2.2f, false);
+            if (played) blockedCooldown = 10f;
+        }
+
+        public override void PlayDeath()
+        {
+            TryPlaySound("Play_ArtiMika_Defeat", 4.5f, true);
+        }
+
+        public override void PlayHurt(float percentHPLost)
+        {
+            if (percentHPLost >= 0.1f)
+            {
+                TryPlaySound("Play_ArtiMika_TakeDamage", 0f, false);
+            }
+        }
+
+        public override void PlayJump() { }
+
+        public override void PlayLevelUp()
+        {
+            if (levelCooldown > 0f) return;
+            bool played = TryPlaySound("Play_ArtiMika_LevelUp", 10f, false);
+            if (played) levelCooldown = 60f;
+        }
+
+        public override void PlayLowHealth() { }
+
+        public override void PlayPrimaryAuthority() { }
+
+        public override void PlaySecondaryAuthority() {}
+
+        public override void PlaySpawn()
+        {
+            TryPlaySound("Play_ArtiMika_Spawn", 3f, true);
+        }
+
+        public override void PlaySpecialAuthority()
+        {
+            if (specialCooldown > 0f) return;
+            bool played = TryPlayNetworkSound(nseBuffSelf, 1.7f, false);
+            if (played) specialCooldown = 20f;
+        }
+
+        public override void PlayTeleporterFinish()
+        {
+            TryPlaySound("Play_ArtiMika_Victory", 4f, false);
+        }
+
+        public override void PlayTeleporterStart()
+        {
+            TryPlaySound("Play_ArtiMika_TeleporterStart", 4.2f, false);
+        }
+
+        public override void PlayUtilityAuthority()
+        {
+            if (utilityCooldown > 0f) return;
+            bool played = TryPlayNetworkSound(nseIceWall, 1.8f, false);
+            if (played) utilityCooldown = 20f;
+        }
+
+        public override void PlayVictory()
+        {
+            TryPlaySound("Play_ArtiMika_Victory", 4f, true);
+        }
+
+        protected override void Inventory_onItemAddedClient(ItemIndex itemIndex)
+        {
+            base.Inventory_onItemAddedClient(itemIndex);
+            if (ArtiMikaVoiceoverComponent.ScepterIndex != ItemIndex.None && itemIndex == ArtiMikaVoiceoverComponent.ScepterIndex)
+            {
+                PlayAcquireScepter();
+            }
+            else
+            {
+                ItemDef id = ItemCatalog.GetItemDef(itemIndex);
+                if (itemIndex == RoR2Content.Items.Squid.itemIndex || itemIndex == RoR2Content.Items.Plant.itemIndex)
+                {
+                    PlayBadItem();
+                }
+                else if (id && id.deprecatedTier == ItemTier.Tier3)
+                {
+                    PlayAcquireLegendary();
+                }
+            }
+        }
+
+        public void PlayAcquireScepter()
+        {
+            if (acquiredScepter) return;
+            TryPlaySound("Play_ArtiMika_AcquireScepter", 19f, false);
+            acquiredScepter = true;
+        }
+
+        public void PlayAcquireLegendary()
+        {
+            TryPlaySound("Play_ArtiMika_Relationship", 9f, false);
+        }
+
+        public void PlayBadItem()
+        {
+            TryPlaySound("Play_ArtiMika_Cafe2", 4.2f, false);
+        }
+
+        //Called via hook
+        public void PlayShrineOfChanceFailServer()
+        {
+            if (!NetworkServer.active || shrineOfChanceFailCooldown > 0f) return;
+            if (Util.CheckRoll(15f))
+            {
+                bool played = TryPlayNetworkSound(nseShrineFail, 2.7f, false);
+                if (played) shrineOfChanceFailCooldown = 60f;
+            }
+        }
+
+        //Called via hook
+        public void PlayBeginCharge()
+        {
+            TryPlaySound("Play_ArtiMika_Buffed", 0f, false);
+        }
+
+        //Called via hook
+        public void PlayReleaseCharge()
+        {
+            TryPlaySound("Play_ArtiMika_Shout", 0f, false);
+        }
+    }
+}
